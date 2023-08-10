8b159df3673fafbabfe7e5cf8250c2ad8c06cd53
omair <omairdal@gmail.com>
2023-08-10 17:47:06 +0300

    some UI work

--- SalesViewPCF/components/App.tsx
+++ SalesViewPCF/components/App.tsx
@@ -1,6 +1,7 @@
 import { MessageBar, MessageBarType, Spinner, Stack, Text } from '@fluentui/react';
 import { observer } from 'mobx-react';
 import React, { useEffect } from 'react';
+import { axa_salesfulfillmentstatusMetadata } from '../cds-generated/entities/axa_SalesFulfillmentStatus';
 import { axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus } from '../cds-generated/enums/axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus';
 import ServiceProvider from '../ServiceProvider';
 import ContextProvider from '../viewModel/context';
@@ -38,14 +39,14 @@ const App = ({ serviceProvider }: props) => {
                     {vm.errorMessage}
                   </MessageBar>
                 )}
-                <Stack horizontal styles={{ root: { overflow: 'scroll' } }}>
-                  <table>
+                <Stack horizontal styles={{ root: { padding: '0 0 0 1rem', height: "100%", width: '100%', overflow: 'scroll' } }}>
+                  <table >
                     <thead>
                       <tr>
-                        <th style={{ height: 0 }}> <Stack styles={{ root: { padding: '0.5rem 1rem', height: '100%' } }} /></th>
+                        <th style={{ height: 0 }}><Stack styles={{ root: { padding: '0.2rem 1rem', height: '100%' } }} /></th>
                         {vm.Departments.map((d) => (
                           <th key={d} style={{ height: 0 }}>
-                            <Stack styles={{ root: { padding: '0.5rem 1rem', border: '1px solid #ccc', height: '100%' } }}>
+                            <Stack styles={{ root: { padding: '0.2rem 1rem', border: '1px solid #fff', height: '100%', backgroundColor: '#eee' } }}>
                               <Text >{d}</Text>
                             </Stack>
                           </th>
@@ -54,21 +55,47 @@ const App = ({ serviceProvider }: props) => {
                     </thead>
                     <tbody>
                       {vm.SFS.map((sf) => (
-                        <tr key={sf.id}>
+                        <tr
+                          key={sf.id}
+                        >
                           <td style={{ height: 0 }}>
-                            <Stack styles={{ root: { padding: '0.5rem 1rem', border: '1px solid #ccc', height: '100%' } }}>
+                            <Stack
+                              verticalAlign="center"
+                              onDoubleClick={() => {
+                                vm.context.navigation.openForm({
+                                  entityName: axa_salesfulfillmentstatusMetadata.logicalName,
+                                  entityId: sf.id
+                                })
+                              }}
+                              styles={{
+                                root: {
+                                  padding: "0.2rem 1rem",
+                                  border: "1px solid #fff",
+                                  backgroundColor: '#eee',
+                                  height: "100%",
+                                  userSelect: "none",
+                                  cursor: "pointer"
+                                }
+                              }}>
                               <Text>{sf.title}</Text>
                             </Stack>
                           </td>
                           {vm.Departments.map((d) => {
                             const status = sf.department[d] || axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted;
-                            const statusColor = status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted ? 'red' :
+                            const statusColor = status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted ? "red" :
                               status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.WIP ? "orange" : "green"
                             const statusText = status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted ? 'Not started' :
                               status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.WIP ? "WIP" : "Done"
                             return (
                               <td key={d} style={{ height: 0 }} >
-                                <Stack verticalFill styles={{ root: { padding: '0.5rem 1rem', border: '1px solid #ccc', backgroundColor: statusColor, height: '100%' } }}>
+                                <Stack verticalAlign="center" horizontalAlign="center" styles={{
+                                  root: {
+                                    padding: '0.2rem 1rem',
+                                    border: '1px solid #fff',
+                                    backgroundColor: statusColor,
+                                    height: '100%'
+                                  }
+                                }}>
                                   <Text styles={{ root: { color: "white" } }}>{statusText}</Text>
                                 </Stack>
                               </td>
@@ -83,53 +110,11 @@ const App = ({ serviceProvider }: props) => {
             )}
           </>
         </ContextProvider >
-      )}
+      )
+      }
     </>
   )
 }
 
 export default observer(App);
 
-
-
-
-
-
-                  // <Stack horizontal>
-                  //   <div style={{ padding: '0.5rem 1rem', border: '1px solid #ccc', width: '150px' }}>
-                  //   </div>
-                  //   {
-                  //     vm.Departments.map((d, i) => (
-                  //       <div className='header-element' style={{ padding: '0.5rem 1rem', border: '1px solid #ccc' }}>
-                  //         <Text variant='medium'>{d}</Text>
-                  //       </div>
-                  //     ))
-                  //   }
-                  // </Stack>
-                  // {
-                  //   vm.SFS.map((sfs) => {
-                  //     const depStatuses = vm.Departments.map((d, i) => {
-                  //       const status = sfs.department[d] || axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted;
-                  //       const statusColor = status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted ? 'red' :
-                  //         status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.WIP ? "yellow" : "green"
-                  //       const statusText = status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted ? 'Not started' :
-                  //         status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.WIP ? "WIP" : "Done"
-                  //       return (
-                  //         <Stack styles={{ root: { padding: '0.5rem 1rem', border: '1px solid #ccc', backgroundColor: statusColor, width: headerWidths[i] } }}>
-                  //           <Text variant='medium'>
-                  //             {statusText}
-                  //           </Text>
-                  //         </Stack>
-                  //
-                  //       )
-                  //     })
-                  //     return (
-                  //       <Stack horizontal >
-                  //         <div style={{ padding: '0.5rem 1rem', border: '1px solid #ccc', width: '150px' }}>
-                  //           <Text variant='medium'>{sfs.title}</Text>
-                  //         </div>
-                  //         {depStatuses}
-                  //       </Stack>
-                  //     )
-                  //   })
-                  // }
