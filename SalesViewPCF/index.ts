15e57f0d053075e06066a1dacf29b3c768f3b379
omair <omairdal@gmail.com>
2023-10-24 17:27:49 +0000

    versioning

--- SalesViewPCF/index.ts
+++ SalesViewPCF/index.ts
@@ -33,7 +33,7 @@ export class SalesViewPCF
     _state: ComponentFramework.Dictionary,
     container: HTMLDivElement
   ): void {
-    console.info("Version 0.0.11");
+    console.info("Version 0.0.12");
     this.context = context;
     this.container = container;
     this.serviceProvider = new ServiceProvider();
