01e69ebcb556a06c90cdddb5c9b109c3bfc6e71c
Omer Dillmann <Odillmann@axadvisory.com>
2024-03-18 22:21:50 +0300

    swap 2 columns

--- SalesViewPCF/components/BodyRows.tsx
+++ SalesViewPCF/components/BodyRows.tsx
@@ -14,93 +14,39 @@ const BodyRows = ({ Departments, sf }: props) => {
           status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.WIP ? "orange" : "#00c000"
         const statusText = status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.Notstarted ? 'Not started' :
           status === axa_departmentfulfillmentstatus_axa_departmentfulfillmentstatus_axa_fulfillmentstatus.WIP ? "WIP" : "Done"
-        return (
-          <td key={d} style={{ height: 0, whiteSpace: 'nowrap' }} >
-            <Stack verticalAlign="center" horizontalAlign="center" styles={{
-              root: {
-                padding: '0.6rem 1rem',
-                border: '2px solid #fff',
-                borderRadius: '3px',
-                backgroundColor: statusColor,
-                height: '100%'
-              }
-            }}>
-              <Text styles={{ root: { color: "white" } }}>{statusText}</Text>
-            </Stack>
-          </td>
+        return (<td key={d} style={{ height: 0, whiteSpace: 'nowrap' }} >
+          <Stack verticalAlign="center" horizontalAlign="center" styles={{
+            root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', backgroundColor: statusColor, height: '100%' }
+          }}>
+            <Text styles={{ root: { color: "white" } }}>{statusText}</Text>
+          </Stack>
+        </td>
         )
       })}
       <td />
       <td style={{ height: 0, whiteSpace: 'nowrap' }} >
-        <Stack
-          verticalAlign="center"
-          horizontalAlign="center"
-          styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }}
-        >
-          <div
-            style={{
-              borderRadius: '50%',
-              backgroundColor: sf.requirements.MDC ? "#00c000" : "#cc0000",
-              height: '25px',
-              width: '25px',
-            }} />
+        <Stack verticalAlign="center" horizontalAlign="center" styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }} >
+          <div style={{ borderRadius: '50%', backgroundColor: sf.requirements.SA ? "#00c000" : "#cc0000", height: '25px', width: '25px', }} />
         </Stack>
       </td>
       <td style={{ height: 0, whiteSpace: 'nowrap' }} >
-        <Stack
-          verticalAlign="center"
-          horizontalAlign="center"
-          styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }}
-        >
-          <div
-            style={{
-              borderRadius: '50%',
-              backgroundColor: sf.requirements.SA ? "#00c000" : "#cc0000",
-              height: '25px',
-              width: '25px',
-            }} />
+        <Stack verticalAlign="center" horizontalAlign="center" styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }} >
+          <div style={{ borderRadius: '50%', backgroundColor: sf.requirements.MDC ? "#00c000" : "#cc0000", height: '25px', width: '25px', }} />
         </Stack>
       </td>
       <td style={{ height: 0, whiteSpace: 'nowrap' }} >
-        <Stack
-          verticalAlign="center"
-          horizontalAlign="center"
-          styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }}
-        >
-          <div style={{
-            borderRadius: '50%',
-            backgroundColor: sf.requirements.DG ? "#00c000" : "#cc0000",
-            height: '25px',
-            width: '25px',
-          }} />
+        <Stack verticalAlign="center" horizontalAlign="center" styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }} >
+          <div style={{ borderRadius: '50%', backgroundColor: sf.requirements.DG ? "#00c000" : "#cc0000", height: '25px', width: '25px', }} />
         </Stack>
       </td>
       <td style={{ height: 0, whiteSpace: 'nowrap' }} >
-        <Stack
-          verticalAlign="center"
-          horizontalAlign="center"
-          styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }}
-        >
-          <div style={{
-            borderRadius: '50%',
-            backgroundColor: sf.requirements.DSR ? "#00c000" : "#cc0000",
-            height: '25px',
-            width: '25px',
-          }} />
+        <Stack verticalAlign="center" horizontalAlign="center" styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }} >
+          <div style={{ borderRadius: '50%', backgroundColor: sf.requirements.DSR ? "#00c000" : "#cc0000", height: '25px', width: '25px', }} />
         </Stack>
       </td>
       <td style={{ height: 0, whiteSpace: 'nowrap' }} >
-        <Stack
-          verticalAlign="center"
-          horizontalAlign="center"
-          styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }}
-        >
-          <div style={{
-            borderRadius: '50%',
-            backgroundColor: sf.requirements.CWS ? "#00c000" : "#cc0000",
-            height: '25px',
-            width: '25px',
-          }} />
+        <Stack verticalAlign="center" horizontalAlign="center" styles={{ root: { padding: '0.6rem 1rem', border: '2px solid #fff', borderRadius: '3px', height: '100%' } }} >
+          <div style={{ borderRadius: '50%', backgroundColor: sf.requirements.CWS ? "#00c000" : "#cc0000", height: '25px', width: '25px', }} />
         </Stack>
       </td>
     </>
