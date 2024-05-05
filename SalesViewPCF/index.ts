e048482950150b3fe95ced2b7945af120ca6f202
Omer Dillmann <Odillmann@axadvisory.com>
2024-05-05 15:24:59 +0300

    changed color

--- SalesViewPCF/index.ts
+++ SalesViewPCF/index.ts
@@ -33,7 +33,7 @@ export class SalesViewPCF
     _state: ComponentFramework.Dictionary,
     container: HTMLDivElement
   ): void {
-    console.info("Version 0.0.25");
+    console.info("Version 0.0.26");
     this.context = context;
     this.container = container;
     this.serviceProvider = new ServiceProvider();
