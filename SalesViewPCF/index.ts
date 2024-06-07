520378a3363f819b46534fa3c35b85a638a956de
Omer Dillmann <Odillmann@axadvisory.com>
2024-06-07 20:29:46 +0300

    versioning

--- SalesViewPCF/index.ts
+++ SalesViewPCF/index.ts
@@ -33,7 +33,7 @@ export class SalesViewPCF
     _state: ComponentFramework.Dictionary,
     container: HTMLDivElement
   ): void {
-    console.info("Version 0.0.29");
+    console.info("Version 0.0.30");
     this.context = context;
     this.container = container;
     this.serviceProvider = new ServiceProvider();
